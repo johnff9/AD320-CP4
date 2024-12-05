4ed9864ee24eceb165e08b75123a18d5639393bc
Margaret Keu <mkeu0526@gmail.com>
2024-12-04 16:17:51 -0800

    Merge fix 2

--- CP4_V2/src/components/ProductDetail.jsx
+++ CP4_V2/src/components/ProductDetail.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Products from "../Products";
 
 const ProductDetail = () => {
